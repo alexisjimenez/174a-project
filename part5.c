575b4f3130510a89bdc90981183e714846352846
vivianescalante <vivianescalante@users.noreply.github.com>
2015-12-05 01:11:12 -0800

    create part5.c
    Splits and checks if it has at least 2 arguments. Also checks if command is exit then breaks. If not exit then it places argument inside file and goes to the python file.

---
+++ part5.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+typedef int bool;
+#define true 1
+#define false 0
+
+int main()
+{
+    bool moreInputs = true;
+
+	while (moreInputs)
+    {
+    	//make char array that will store the user input 
+    	char commArray[256], backUp[256];
+
+    	//ask user what they want to do 
+    	printf("Please enter the task you would like to perform: ");
+	    fgets(commArray, 256, stdin);
+	    int len = strlen(commArray);
+	    printf("Length of input: %i\n", len);
+        printf("The User Input: %s\n", commArray);
+
+        strncpy(backUp, commArray, len);
+        printf("This is in backUp: %s\n", backUp);
+
+        int exitLC = strcmp(commArray,"exit\n");
+        printf("exitLC: %i\n", exitLC);
+        int EXITUC = strcmp(commArray,"EXIT\n");
+        printf("EXITUC: %i\n", EXITUC);
+
+        printf("This is in commArray: %s\n", commArray);
+        char *splitInput = strtok(commArray," ");
+        printf("This is in commArray: %s\n", commArray);
+        int splitLen;
+        while(splitInput!=NULL)
+        {
+            printf("%s\n", splitInput);
+            splitLen++;
+            splitInput = strtok(NULL, " ");
+        }
+        
+
+        //the length of the split one  
+        printf("This is the length of splitInput: %i\n", splitLen);
+
+        if (splitLen <= 1)
+        {
+            printf("Incorrect number of arguments. ");
+            moreInputs = true;
+        }
+        else 
+        {
+            if (exitLC == 0 || EXITUC == 0)
+            {
+                break;
+            }
+            else
+            {
+                FILE *f; 
+                f = fopen ("userInput.txt", "w");
+                fscanf(f,"%s\n",backUp);
+                fputs(backUp,f);
+                fclose(f);
+                printf("File write was successful\n"); 
+                   
+                system("python part5.py");
+            }
+        }
+    }
+}
