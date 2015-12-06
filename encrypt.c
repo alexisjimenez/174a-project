a6ee48bde2a269a92913aaca208c53e3f05bd58c
Alexis Jimenez <alexisjimenez@umail.ucsb.edu>
2015-12-06 13:40:33 -0800

    updated

--- encrypt.c
+++ encrypt.c
@@ -6,16 +6,18 @@
 
 int main (int argc, char* argv[]){
 
-    paillier_plaintext_t* amt = paillier_plaintext_from_ui( atoi(argv[1]));
+  //aillier_plaintext_t* amt = paillier_plaintext_from_ui( atoi(argv[1]));
                                                                                                                
     paillier_pubkey_t* publicKey;
     publicKey = paillier_pubkey_from_hex("91b6a0c0e8374bac6d4e75f2497ff89f") ;
     
-
+    unsigned int salary = strtoul(argv[1], NULL, 0);
+    paillier_plaintext_t* amt = paillier_plaintext_from_ui( salary);
    paillier_ciphertext_t* encr;
    encr = paillier_enc(NULL, publicKey,amt,paillier_get_rand_devurandom); 
    char* result_string = mpz_get_str(NULL, 10, encr->c);
    printf("%s\n",result_string);
-				    
+ 
+   return 0;				    
 
 }
