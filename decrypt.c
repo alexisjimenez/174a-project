910e7d3ddf7766e000081928236383ff34927d54
Alexis Jimenez <alexisjimenez@umail.ucsb.edu>
2015-12-05 22:12:34 -0800

    updated

--- decrypt.c
+++ decrypt.c
@@ -4,14 +4,10 @@
 #include <gmp.h>
 #include "paillier.c"
 int main (int argc, char* argv[]){
-  if( argc == 2 ) {
-
     //GET ENCRYPTED VALUE
     paillier_ciphertext_t* eAmt = (paillier_ciphertext_t*) malloc(sizeof(paillier_ciphertext_t));
     mpz_init_set_str(eAmt->c,argv[1],10);
  
-   gmp_printf("The encrypted amount supplied is %Zd\n", eAmt->c);
-   
    //GET PUBLIC AND PRIVATE KEY  
     paillier_pubkey_t* publicKey;
     publicKey = paillier_pubkey_from_hex("91b6a0c0e8374bac6d4e75f2497ff89f") ;
@@ -21,19 +17,7 @@ int main (int argc, char* argv[]){
 
     //DECRYPTION
    paillier_plaintext_t* decr;
-   decr = paillier_dec(NULL,publicKey, privateKey,eAmt10192720829540281984164035358194123504309349354931037587226740646499064729134); 
+   decr = paillier_dec(NULL,publicKey, privateKey,eAmt); 
    int dAmt = mpz_get_ui(decr->m);
-   printf("The decrypted value is : %d\n",dAmt);
-
-      
-  }
-  else if( argc > 2 ) {
-    printf("Too many arguments supplied.\n");
-  }
-  else {
-    printf("One argument expected.\n");
-  }
-
-
-
+   printf("%d\n",dAmt);
 }
--- encrypt.c
+++ encrypt.c
@@ -5,9 +5,7 @@
 #include "paillier.c"
 
 int main (int argc, char* argv[]){
-  if( argc == 2 ) {
 
-    printf("The salary is: %s\n",argv[1]);
     paillier_plaintext_t* amt = paillier_plaintext_from_ui( atoi(argv[1]));
                                                                                                                
     paillier_pubkey_t* publicKey;
@@ -17,22 +15,7 @@ int main (int argc, char* argv[]){
    paillier_ciphertext_t* encr;
    encr = paillier_enc(NULL, publicKey,amt,paillier_get_rand_devurandom); 
    char* result_string = mpz_get_str(NULL, 10, encr->c);
-   
-   printf("The encrypted value is: %s\n",result_string);
-				     
-   FILE *f;
-   f = fopen("encrypt.txt", "w");
-   gmp_fprintf(f,"%Zd\n",encr);
-   fclose(f);
-    
-  }
-  else if( argc > 2 ) {
-    printf("Too many arguments supplied.\n");
-  }
-  else {
-    printf("One argument expected.\n");
-  }
-
-
+   printf("%s\n",result_string);
+				    
 
 }
