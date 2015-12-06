abf83ce77b7f7632157fefd03913ee72c1dc3e1d
vivianescalante <vivianescalante@users.noreply.github.com>
2015-12-05 16:24:45 -0800

    Update part5.py

--- part5.py
+++ part5.py
@@ -3,6 +3,8 @@ from __future__ import division
 import csv
 import sys
 import string 
+import os 
+
 
 import mysql.connector
 
@@ -24,19 +26,6 @@ try:
 except mysql.connector.Error as err:
   print("Connection Error: {}".format(err))
   sys.exit(1)
-
-#This is to read the file
-def readInputFile(filename, function):
-  #tries to open the file
-  try:
-    f = open(filename, 'r')
-    userInput = f.read(256)
-    print("UserInput from the file: ", userInput)
-    #places values into function as values 
-    function(userInput)
-  #if can't open it then gives error
-  except IOError as err:
-    print("I/O Error: {}".format(err))
 
 #This is what does all the inserting, selecting and stuff 
 def emp_function(values):
@@ -72,33 +61,46 @@ def emp_function(values):
         print("Got inside SELECT *")
         """TO DO: PRINT OUT EVERYTHING"""
         #insert_employee = ("SELECT * FROM Employees")
+        #result = cursor.execute(insert_employee, values)
+        #print ("This is the SELECT * FROM Employees: ", result)
       elif (splitInput[1].isdigit() == true):
         print("Got inside SELECT DIGIT")
         """TO DO: CHECK IF ID IN DATABASE"""
         #select_employee = ("SELECT ? FROM Employees", splitInput[2])
         #cursor.execute(select_employee, values)
         #for row in cursor: 
-          #print row 
+          ##print ("This is the SELECT id FROM Employees: ",row) 
 
     elif (length == 3): 
       print("This is the second command, should be AVG: ", splitInput[1])
-      if (splitInput[1] == 'AVG'):
+      if (splitInput[1] == 'SUM'):
+        print("Got inside SELECT SUM salary")
+        #sum_employee = ("SELECT SUM_HE(salary) FROM Employees") 
+        #sum = execute(sum_employee, values)
+        #print ("This is the SELECT SUM salary: ", sum)
+      elif (splitInput[1] == 'AVG'):
         print("Got inside SELECT AVG salary")
         #sum_employee = ("SELECT SUM_HE(salary) FROM Employees") 
         #count_employee = ("SELECT COUNT (*) FROM Employees")
         #sum = execute(sum_employee, values)
-        #count = execute(count_employee, values)
+        #count = cursor.execute(count_employee, values)
         #average= sum/count
-        #print average
+        #print ("This is the SELECT AVG salary: ", average)
+
 
     elif (length == 4): 
       print("This is the second command, should be SUM or AVG: ", splitInput[1])
       print("This is the third command, should be WHERE : ", splitInput[2])
-      if (splitInput[1] == 'SUM' and splitInput[2] == 'WHERE'): 
+      if (splitInput[1] == '*' and splitInput[2] == 'WHERE'):
+        print("Got inside SELECT * WHERE conditions")
+        #select_employee = ("SELECT * FROM Employees WHERE ?", splitInput[3])
+        #result = cursor.execute(select_employee, values)
+        #print ("This is the SELECT * FROM Employees WHERE condition: ",result)
+      elif (splitInput[1] == 'SUM' and splitInput[2] == 'WHERE'): 
         print("Got inside SELECT SUM WHERE conditions")
         #select_employee = ("SELECT SUM_HE(salary) FROM Employees WHERE ?", splitInput[3]) 
         #result = execute(select_employee, values)
-        #print result
+        #print ("This is the SELECT SUM_HE(salary) FROM Employees WHERE condition: ",result)
       elif (splitInput[1] == 'AVG' and splitInput[2] == 'WHERE'):
         print("Got inside SELECT AVG WHERE conditions")
         #sum_employee = ("SELECT SUM_HE(salary) FROM Employees WHERE ?", splitInput[3]) 
@@ -106,7 +108,7 @@ def emp_function(values):
         #sum = execute(sum_employee, values)
         #count = execute(count_employee, values)
         #average= sum/count
-        #print average
+        #print ("This is the SELECT AVG FROM Employees WHERE condition: ",result)
       else: 
         print ("Incorrect input.")
 
@@ -117,11 +119,16 @@ def emp_function(values):
       print("This is the fourth command, should be BY: ", splitInput[3])
       #checking if it's grouping by age
       print("This is the fifth command, should be age: ", splitInput[4])     
-      if (splitInput[1] == 'SUM' and splitInput[2]== 'GROUP' and splitInput[3]== 'BY' and splitInput[4]== 'age'): 
+      if (splitInput[1] == '*' and splitInput[2] == 'GROUP' and splitInput[3]== 'BY' and splitInput[4]== 'age'):
+        print("Got inside SELECT * GROUP BY age")
+        #select_employee = ("SELECT * FROM Employees GROUP BY age")
+        #result = cursor.execute(select_employee, values)
+        #print ("This is the SELECT * FROM Employees GROUP BY age: ",result)
+      elif (splitInput[1] == 'SUM' and splitInput[2]== 'GROUP' and splitInput[3]== 'BY' and splitInput[4]== 'age'): 
         print("Got inside SELECT SUM GROUP BY age")
         #select_employee = ("SELECT SUM_HE(salary) FROM Employees GROUP BY ?", splitInput[4]) 
         #result = execute(select_employee, values)
-        #print result
+        #print ("This is the SELECT SUM FROM Employees GROUP BY age: ",result)
       elif (splitInput[1] == 'AVG' and splitInput[2]== 'GROUP' and splitInput[3]== 'BY' and splitInput[4]== 'age'):
         print("Got inside SELECT AVG GROUP BY age")
         #sum_employee = ("SELECT SUM_HE(salary) FROM Employees WHERE ? GROUP BY age", splitInput[3]) 
@@ -129,9 +136,10 @@ def emp_function(values):
         #sum = execute(sum_employee, values)
         #count = execute(count_employee, values)
         #averageGroup= sum/count
-        #print averageGroup
+        #print ("This is the AVG FROM Employees GROUP BY age: ",result)
       else: 
           print ("Incorrect input.")
+
     elif (length == 7):
       print("This is the second command, should be SUM or AVG: ", splitInput[1])
       print("This is the third command, should be WHERE or GROUP: ", splitInput[2])
@@ -141,16 +149,34 @@ def emp_function(values):
       print("This is the fifth command, should be BY or HAVING if only GROUP BY: ", splitInput[5])
       #checking if it's grouping by age
       print("This is the sixth command, should be BY or conditions if only GROUP BY: ", splitInput[6])
-      if (splitInput[1] == 'SUM' and splitInput[2]== 'WHERE' and splitInput[4]== 'GROUP' and splitInput[5]== 'BY' and splitInput[6]== 'age'): 
-        print("Got inside SELECT SUM WHERE conditions GROUP BY age")
+      if (splitInput[1] == '*' and splitInput[2]== 'WHERE' and splitInput[4]== 'GROUP' and splitInput[5]== 'BY' and splitInput[6]== 'age'): 
+        print("Got inside SELECT * WHERE conditions GROUP BY age")
         #select_employee = ("SELECT age, SUM_HE(salary) FROM Employees WHERE ? GROUP BY age", splitInput[3])
         #result = execute(select_employee, values)
-        #print result
+        #print ("This is the SELECT SUM WHERE conditions GROUP BY age : ",result)
+      elif (splitInput[1] == '*' and splitInput[2]== 'GROUP' and splitInput[3]== 'BY' and splitInput[4]== 'age' and splitInput[5]== 'HAVING'): 
+        print("Got inside SELECT * WHERE conditions GROUP BY age")
+        #select_employee = ("SELECT age, SUM_HE(salary) FROM Employees GROUP BY age HAVING ?", splitInput[6])
+        #result = execute(select_employee, values)
+        #print ("This is the SELECT SUM GROUP BY age HAVING conditions: ",result)
+      elif(splitInput[1] == 'SUM' and splitInput[2]== 'WHERE' and splitInput[4]== 'GROUP' and splitInput[5]== 'BY' and splitInput[6]== 'age'):
+        print("Got inside SELECT SUM WHERE condition GROUP BY age")
+        #select_employee = ("SELECT age, SUM_HE(salary) FROM Employees WHERE ? GROUP BY age", splitInput[3])
+        #result = execute(select_employee, values)
+        #print ("This is the SELECT SUM WHERE condition GROUP BY age: ",result)
       elif(splitInput[1] == 'SUM' and splitInput[2]== 'GROUP' and splitInput[3]== 'BY' and splitInput[4]== 'age' and splitInput[5]== 'HAVING'):
         print("Got inside SELECT SUM GROUP BY age HAVING conditions")
         #select_employee = ("SELECT age, SUM_HE(salary) FROM Employees GROUP BY age HAVING ?", splitInput[6])
         #result = execute(select_employee, values)
-        #print result
+        #print ("This is the SELECT SUM GROUP BY age HAVING conditions : ",result)
+      elif(splitInput[1] == 'AVG' and splitInput[2]== 'WHERE' and splitInput[4]== 'GROUP' and splitInput[5]== 'BY' and splitInput[6]== 'age'):
+        print("Got inside SELECT AVG WHERE condition GROUP BY age")
+        #sum_employee = ("SELECT SUM_HE(salary) FROM Employees WHERE ? GROUP BY age", splitInput[3]) 
+        #count_employee = ("SELECT COUNT (*) FROM Employees GROUP BY age HAVING ?", splitInput[3])
+        #sum = execute(sum_employee, values)
+        #count = execute(count_employee, values)
+        #averageGroup= sum/count
+        #print ("This is the SELECT AVG GROUP BY age HAVING conditions : ",result)
       elif(splitInput[1] == 'AVG' and splitInput[2]== 'GROUP' and splitInput[3]== 'BY' and splitInput[4]== 'age' and splitInput[5]== 'HAVING'):
         print("Got inside SELECT AVG GROUP BY age HAVING conditions")
         #sum_employee = ("SELECT SUM_HE(salary) FROM Employees GROUP BY age HAVING ?", splitInput[6]) 
@@ -158,7 +184,7 @@ def emp_function(values):
         #sum = execute(sum_employee, values)
         #count = execute(count_employee, values)
         #averageGroup= sum/count
-        #print averageGroupHav
+        #print ("This is the SELECT AVG GROUP BY age HAVING conditions : ",result)
       else: 
         print ("Incorrect input.")
 
@@ -171,12 +197,16 @@ def emp_function(values):
       #checking if it's grouping by age
       print("This is the sixth command, should be age: ", splitInput[6])
       print("This is the seventh command, should be HAVING: ", splitInput[7])
-
-      if (splitInput[1] == 'SUM' and splitInput[2]== 'WHERE' and splitInput[4]== 'GROUP' and splitInput[5]== 'BY' and splitInput[6]== 'age' and splitInput[7]== 'HAVING'): 
+      if (splitInput[1] == '*' and splitInput[2]== 'WHERE' and splitInput[4]== 'GROUP' and splitInput[5]== 'BY' and splitInput[6]== 'age' and splitInput[7]== 'HAVING'): 
+        print("Got inside SELECT * WHERE conditions GROUP BY age HAVING conditions")
+        #select_employee = ("SELECT age, * FROM Employees WHERE ? GROUP BY age HAVING ?", splitInput[3], splitInput[8])
+        #result = execute(select_employee, values)
+        #print ("This is the SELECT * WHERE conditions GROUP BY age HAVING conditions : ",result)
+      elif (splitInput[1] == 'SUM' and splitInput[2]== 'WHERE' and splitInput[4]== 'GROUP' and splitInput[5]== 'BY' and splitInput[6]== 'age' and splitInput[7]== 'HAVING'): 
         print("Got inside SELECT SUM WHERE conditions GROUP BY age HAVING conditions")
         #select_employee = ("SELECT age, SUM_HE(salary) FROM Employees WHERE ? GROUP BY age HAVING ?", splitInput[3], splitInput[8])
-        #result = execute(select_employee, values
-        #print result
+        #result = execute(select_employee, values)
+        #print ("This is the SELECT SUM WHERE conditions GROUP BY age HAVING conditions : ",result)
       elif(splitInput[1] == 'AVG' and splitInput[2]== 'WHERE' and splitInput[4]== 'GROUP' and splitInput[5]== 'BY' and splitInput[6]== 'age' and splitInput[7]== 'HAVING'):
         print("Got inside SELECT AVG WHERE conditions GROUP BY age HAVING conditions")
         #sum_employee = ("SELECT SUM_HE(salary) FROM Employees WHERE ? GROUP BY age HAVING ?", splitInput[3], splitInput[8]) 
@@ -184,19 +214,25 @@ def emp_function(values):
         #sum = execute(sum_employee, values)
         #count = execute(count_employee, values)
         #averageGroup= sum/count
-        #print averageGroupHav
+        #print ("This is the SELECT AVG WHERE conditions GROUP BY age HAVING conditions : ",result)
       else: 
         print ("Incorrect input.")
     else:
         print("Invalid arguments.")
 
 if __name__ == '__main__':
-  #readInputFile(filename, emp_function)
-  readInputFile("userInput.txt", emp_function)
-  #commit data 
-  cnx.commit()
-
+  input_var = raw_input("Please enter your command: ")
+  while True:
+    print ("You entered: " + input_var)
+    if (input_var == 'exit' or input_var == 'EXIT'):
+      break;
+    emp_function(input_var)
+    #commit data 
+    cnx.commit()
+    input_var = raw_input("Please enter your command: ")
   cursor.close()
   cnx.close()
+
+
 
 
